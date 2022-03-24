8e30fa54bde3f61e2993663d931da5e3cbbbaafa
alessandro-raul <alessandro.guedes@bandtec.com.br>
2022-03-24 19:38:26 -0300

    feat: add app project

---
+++ Ink4YouApp/app/src/main/java/com/ink4youapp/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.ink4youapp.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.ink4youapp.data.LoginDataSource
+import com.ink4youapp.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
