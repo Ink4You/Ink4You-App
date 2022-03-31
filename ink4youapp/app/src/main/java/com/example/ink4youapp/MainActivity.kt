212ec395cd6f44b2ab8e5a14d2fc48eadbeb09d6
alessandro-raul <alessandro.guedes@bandtec.com.br>
2022-03-31 20:02:58 -0300

    feat: add login screen

--- ink4youapp/app/src/main/java/com/example/ink4youapp/MainActivity.kt
+++ ink4youapp/app/src/main/java/com/example/ink4youapp/MainActivity.kt
@@ -1,11 +1,51 @@
 package com.example.ink4youapp
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.Window
+import android.view.animation.AnimationUtils
+import android.widget.LinearLayout
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var linear_login: LinearLayout
+    private lateinit var linear_registry: LinearLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+
+        linear_login = findViewById(R.id.linear_login)
+        linear_registry = findViewById(R.id.linear_registry)
     }
+
+    fun showRegistryOptions(view: View) {
+        var animation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+        linear_login.startAnimation(animation)
+        linear_login.visibility = View.GONE
+
+        Handler().postDelayed({
+            animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+            linear_registry.startAnimation(animation)
+            linear_registry.visibility = View.VISIBLE
+        }, 300)
+    }
+
+    fun showLogin(view: View) {
+        var animation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+        linear_registry.startAnimation(animation)
+        linear_registry.visibility = View.GONE
+
+        Handler().postDelayed({
+            animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+            linear_login.startAnimation(animation)
+            linear_login.visibility = View.VISIBLE
+        }, 300)
+    }
+
 }
