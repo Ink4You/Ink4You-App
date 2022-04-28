d080f3ee4d2e4021d092609d98c946561069ea2d
alessandro-raul <alessandro.guedes@bandtec.com.br>
2022-04-28 18:17:46 -0300

    refactor: changing API url from remote

--- ink4youapp/app/src/main/java/com/example/ink4youapp/rest/Rest.kt
+++ ink4youapp/app/src/main/java/com/example/ink4youapp/rest/Rest.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object Rest {
-    val baseUrl = "http://192.168.0.9:8080/"
+    val baseUrl = "http://54.172.224.141:8080/"
     fun getInstance() : Retrofit {
         return Retrofit
             .Builder()
