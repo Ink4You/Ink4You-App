7f8116884663f2d16360c28ffaab38d2535a2346
AlexandreSGJR <alexandre.guedesjr@bandtec.com.br>
2022-05-03 14:50:18 -0300

    WIP - Added recycleer view of recents tattoos

--- ink4youapp/app/src/main/java/com/example/ink4youapp/ExploreFragment.kt
+++ ink4youapp/app/src/main/java/com/example/ink4youapp/ExploreFragment.kt
@@ -5,6 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ink4youapp.adapters.TatuagemDtoAdpter
+import com.example.ink4youapp.models.fakeTatuagens
 
 class ExploreFragment : Fragment() {
 
@@ -13,6 +17,12 @@ class ExploreFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        var view = inflater.inflate(R.layout.fragment_explore, container, false)
+
+        val recyclerViewNewTattoos = view.findViewById<RecyclerView>(R.id.recyclerViewNewTattoos);
+        recyclerViewNewTattoos.adapter = TatuagemDtoAdpter(fakeTatuagens());
+        recyclerViewNewTattoos.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false);
+
+        return view;
     }
 }
