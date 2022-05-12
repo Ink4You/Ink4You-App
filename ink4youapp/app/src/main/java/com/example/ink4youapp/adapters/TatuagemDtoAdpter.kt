952939443da953d7a432831e06d526533acbda87
AlexandreSGJR <alexandre.guedesjr@bandtec.com.br>
2022-05-11 21:35:31 -0300

    add banner and corrections on RecyclerViews

--- ink4youapp/app/src/main/java/com/example/ink4youapp/adapters/TatuagemDtoAdpter.kt
+++ ink4youapp/app/src/main/java/com/example/ink4youapp/adapters/TatuagemDtoAdpter.kt
@@ -1,5 +1,6 @@
 package com.example.ink4youapp.adapters
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
@@ -8,12 +9,17 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.marginLeft
 import androidx.recyclerview.widget.RecyclerView
 import com.example.ink4youapp.R
+import com.example.ink4youapp.TattooDetails
+import com.example.ink4youapp.UserRegistry
 import com.example.ink4youapp.models.TatuagemDTO
 import java.io.InputStream
 import java.net.URL
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.ContextCompat.startActivity
 
 class TatuagemDtoAdpter(val tatuagens: MutableList<TatuagemDTO>) : RecyclerView.Adapter<TatuagemDtoAdpter.TatuagemViewHolder>() {
 
@@ -31,8 +37,13 @@ class TatuagemDtoAdpter(val tatuagens: MutableList<TatuagemDTO>) : RecyclerView.
     inner class TatuagemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(tatuagem: TatuagemDTO) {
             with(tatuagem) {
+                itemView.marginLeft
                 itemView.findViewById<TextView>(R.id.tv_title).text = titulo.toString();
                 itemView.findViewById<TextView>(R.id.tv_artist).text = nome;
+                itemView.setOnClickListener { view ->
+                    val intent = Intent(view.context, TattooDetails::class.java)
+                    startActivity(view.context,intent, null);
+                }
             }
         }
     }
